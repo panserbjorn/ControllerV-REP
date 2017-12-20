cce9feba5d009394f7a565229cd436459e2e5795
Joaquín Silveira <joaco.silveira@gmail.com>
2017-12-20 14:11:34 -0300

    Se agrega un nuevo script script4 que toma no solo las 10 mejores sino 5 random. Además utiliza un generador de secuencias que modifica los movimiento también.

---
+++ secuenceGenerator2.py
@@ -0,0 +1,43 @@
+'''
+Acá en este archivo voy a hacer un conjunto de funciones 
+que lo que hagan es multiplicar las líneas de secuencias del archivo de texto que tengo ahora.
+Así que lo que van a lograr es dada una secuencia y un archivo en el cual van a escupir la salida, 
+modifican aleatoriamente los tiempos de esa secuencia unas 100 veces guardandolas en una lista ignorando 
+los repetidos para poder luego guardar en el archivo esas secuencias.
+
+'''
+
+import numpy as np
+import functools
+
+def generateNewSec(secuence, numberOfSec):
+	newSecuences = []
+	while len(newSecuences) < numberOfSec:
+		nextSec = []
+		for instruct in secuence:
+			movement = instruct[0]
+			time = instruct[1]
+			timeModification = np.random.randint(-5,5)
+			movementmodification = np.random.randint(-5,5)
+			nextSec.append((abs((movement+movementmodification)%10),abs(time+timeModification)))
+		newSecuences.append(nextSec)
+	return newSecuences
+
+
+def recordSecuences (newSecuences, fileName):
+	f = open(fileName, "w")
+	halfParsed = list(map( lambda sec: list(map(lambda instr: str(instr[0]) + '-' + str(instr[1]),sec)),newSecuences))
+	fullParsed = list(map(lambda hsec:functools.reduce(lambda b,x:b + ',' + x,hsec),halfParsed))
+	for i in fullParsed:
+		f.write(i)
+		f.write('\n')
+	f.close()
+
+
+def recordRunOutput(runInfo, fileName):
+	f = open(fileName, "w")
+	for i in runInfo:
+		f.write(str(i[0]) + "," + str(i[1]))
+		f.write('\n')
+	f.close()
+
