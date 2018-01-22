814ec475533f7a899aaa79b4e26afc553600f118
Joaquín Silveira <joaco.silveira@gmail.com>
2018-01-22 16:49:43 -0300

    comentarios sobre cambios que debo realizar en myEnv

--- Final/PatrikCoadyPPO/mySimulatedEnv.py
+++ Final/PatrikCoadyPPO/mySimulatedEnv.py
@@ -6,6 +6,11 @@ class myEnv():
 	score
 	hasfallen
 
+	'''
+	TOOD debería conectarme con el simulador en el init
+	Así de esta manera puedo controlar las cosas y poder devolver lo que hacen los otro métodos
+	
+	'''
 	def _init_():
 		self.portNumb = 19997
 		actions = []
@@ -14,16 +19,20 @@ class myEnv():
 
 	'''
 	Este método debería devolver las posiciones y las velocidades de las partes del robot, no la cantidad de variables a devolver
-	
+
 	'''
 	def observation_space():
 		#Son las 4 posiciones, más las 4 velocidades de los motores
 		#Más la posición de la cabez y la velocidad de la cabeza en el eje x
 		return 10
 
+	'''
+	Este método debería devolver las acciones posibles en lugar de solo la cantidad de acciones que se pueden realizar
+	'''
 	def action_space():
 		#Son las 64 configuraciones que pueden tener los motres (son 4 motores y 3 estados [apagado, encendido positivo, encendido negativo], así que es 4^3)
-		return 4^3
+		#return 4^3
+		return range(4^3)
 
 
 	'''
